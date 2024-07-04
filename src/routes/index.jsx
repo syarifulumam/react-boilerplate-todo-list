d555208d47569bf688b3244b3ac2a1f601a8a8f2
syariful umam <syarifulumam7@gmail.com>
2024-07-04 10:57:44 +0700

    fix : route index

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,20 @@
 import MainLayout from '@layouts/MainLayout';
 import TodoLayout from '@layouts/TodoLayout';
 
-import Home from '@pages/Home';
+// import Home from '@pages/Home';
 import NotFound from '@pages/NotFound';
 import Todo from '@pages/Todo';
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   protected: false,
+  //   component: Home,
+  //   layout: MainLayout,
+  // },
   {
     path: '/',
-    name: 'Home',
-    protected: false,
-    component: Home,
-    layout: MainLayout,
-  },
-  {
-    path: '/todo',
     name: 'Todo',
     protected: false,
     component: Todo,
